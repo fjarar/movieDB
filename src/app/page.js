a49669173c3a46ef4654f25335cc6ec0c53d911f
fjarar <flavio.jara.rebolledo@gmail.com>
2024-08-07 12:54:23 -0400

    added results component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
+import Results from '@/components/Results';
 import React from 'react';
 
-const Home = () => {
+const API_KEY = process.env.API_KEY;
+
+async function Home({searchParams}) {
+  const genre = searchParams.genre || 'fetchTrending';
+  console.log(genre);
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+
+  const data = await res.json();
+  if(!res.ok){
+    throw new Error('Failed to fetch data')
+  }
+
+  const results = data.results;
+  
   return (
     <div>
-      <h1>Home</h1>
+      <Results results={results}/>
     </div>
   );
 }

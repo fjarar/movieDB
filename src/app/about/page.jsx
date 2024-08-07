2e22f33a16cffd5ac18f8afe74718d87f1386a6c
fjarar <flavio.jara.rebolledo@gmail.com>
2024-08-07 10:47:52 -0400

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a rhoncus
+        est. Nullam pellentesque eget velit non lobortis. Curabitur eget odio
+        diam. Praesent non maximus dolor, non consectetur mi. Curabitur
+        malesuada mauris eu augue dapibus tempor. Morbi justo elit, gravida nec
+        purus ac, elementum rhoncus nisl. Vestibulum vulputate auctor dui, sit
+        amet tempor elit porta at. Donec libero lacus, elementum id augue sed,
+        vestibulum accumsan justo. Pellentesque pellentesque, purus a faucibus
+        euismod, sem nisi scelerisque enim, quis ornare lacus felis ac lacus.
+        Nulla facilisi. Integer dictum eget leo nec tempus. Etiam ornare
+        faucibus justo, a blandit quam dictum vitae. Ut ornare magna justo, nec
+        posuere libero posuere sed. Duis tincidunt, odio mollis lacinia ornare,
+        enim ligula sodales massa, aliquet tempor enim magna nec leo. Nam
+        ullamcorper neque purus.
+      </p>
+      <p>
+        Maecenas pellentesque vel sem ac lobortis. Suspendisse potenti. Ut ut
+        consectetur massa, ut dignissim tellus. Donec vestibulum quam ante, et
+        aliquam nulla euismod ut. Interdum et malesuada fames ac ante ipsum
+        primis in faucibus. Suspendisse sed rutrum ipsum, vel dapibus urna. In
+        vestibulum mi et placerat laoreet. Aliquam ipsum felis, consectetur
+        eleifend placerat quis, placerat id dolor. Sed porttitor at nulla in
+        sollicitudin. Integer pretium varius arcu ac malesuada.
+      </p>
+      <p>
+        Suspendisse condimentum iaculis libero, sit amet volutpat diam rutrum
+        vel. Nunc at ligula urna. Cras fermentum quis libero a rhoncus. Fusce
+        lacus ex, elementum at leo vel, cursus elementum metus. Nullam vel eros
+        vehicula, porta risus vitae, accumsan risus. Praesent viverra volutpat
+        ligula, elementum pellentesque sapien vestibulum in. Donec congue tellus
+        quis sem ultricies, eget suscipit elit aliquet. Curabitur iaculis sapien
+        ipsum, non facilisis erat consectetur eget. Nulla facilisis eu massa
+        vitae sollicitudin. Etiam ultrices scelerisque quam, in lacinia quam
+        fringilla et. Aliquam lectus dui, rhoncus vitae ultricies sed, ornare
+        eget turpis. Proin non dui nibh. Quisque eget ex sit amet quam tincidunt
+        cursus. Aliquam aliquam sapien ut nisl rhoncus, sit amet tempus nisl
+        sagittis.
+      </p>
+    </div>
+  );
+};
+
+export default Page;

254118e1d5513a1bcbb0ab5d91d1454792970d9a
fjarar <flavio.jara.rebolledo@gmail.com>
2024-08-07 20:40:51 -0400

    added movie page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,6 +13,7 @@ const Card = ({ result }) => {
           }`}
           width={500}
           height={300}
+          alt={`${result.title}`}
           className="sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300"
         ></Image>
         <div className="p-2">

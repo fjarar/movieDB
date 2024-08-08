cba768f8e57ea6a64bda1f93ddad8c388c72b724
fjarar <flavio.jara.rebolledo@gmail.com>
2024-08-07 21:26:38 -0400

    added search component

---
+++ src/app/search/[searchTerm]/page.jsx
@@ -0,0 +1,22 @@
+import Results from "@/components/Results";
+import React from "react";
+
+const SearchPage = async ({ params }) => {
+  const searchTerm = params.searchTerm;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&include_adult=false&language=en-US`
+  );
+  const data = await res.json();
+  const results = data.results;
+  return (
+    <div>
+      {results && results.length > 1 ? (
+        <Results results={results} />
+      ) : (
+        <h1 className="text-center pt-6">No results found</h1>
+      )}
+    </div>
+  );
+};
+
+export default SearchPage;

cb1e8c1a6e69da9c26eafb496dfc8f0ee0b4ad84
fjarar <flavio.jara.rebolledo@gmail.com>
2024-08-08 12:54:06 -0400

    fix an style problem with navitems

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
     return (
         <div className='flex justify-center'>
-            <img className='h-52' src="loading.svg" alt="loading image" />
+            <img className='h-52' src="loading.svg" alt="loading image"/>
         </div>
     );
 }
--- src/components/Navbaritem.jsx
+++ src/components/Navbaritem.jsx
@@ -1,30 +1,42 @@
 "use client";
 
 import Link from "next/link";
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
-function Search(){
+function Search({ param, setActive }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
-  return genre;
+
+  useEffect(() => {
+    if (genre === param) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  }, [genre, param, setActive]);
+
+  return null;
 }
 
 const Navbaritem = ({ title, param }) => {
+  const [isActive, setActive] = useState(false);
+
   return (
     <div>
-      <Suspense>
-        <Link
-          className={`hover:text-amber-600 font-semibold ${
-            <Suspense><Search /></Suspense> === param
-              ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
-              : ""
-          }`}
-          href={`/?genre=${param}`}
-        >
-          {title}
-        </Link>
+      <Suspense fallback={null}>
+        <Search param={param} setActive={setActive}/>
       </Suspense>
+      <Link
+        className={`hover:text-amber-600 font-semibold ${
+          isActive
+            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+            : ""
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
     </div>
   );
 };

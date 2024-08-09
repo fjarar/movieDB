1a581ee41715bbc1c5b19d17cbe28741113396d7
fjarar <flavio.jara.rebolledo@gmail.com>
2024-08-08 20:02:12 -0400

    added footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Providers from "./Providers";
 import Navbar from "@/components/Navbar";
 import SearchBox from "@/components/SearchBox";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,13 +16,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} relative`}>
         <Providers>
           <Header />
           <Navbar />
           <SearchBox />
-          {children}
+          {children}          
         </Providers>
+        <Footer />
       </body>
     </html>
   );
